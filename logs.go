d81ad51f8abe38546e892dcddc4802ce464902bc
zgame <premwit.min@gmail.com>
2025-06-14 15:55:39 +0700

    fix :

--- logs.go
+++ logs.go
@@ -4,15 +4,15 @@ import "fmt"
 
 func Info(param ...any) {
 	param = append([]any{"Info"}, param...)
-	fmt.Println(param)
+	fmt.Println(param...)
 }
 
 func Error(param ...any) {
 	param = append([]any{"Error"}, param...)
-	fmt.Println(param)
+	fmt.Println(param...)
 }
 
 func Warn(param ...any) {
 	param = append([]any{"Warn"}, param...)
-	fmt.Println(param)
+	fmt.Println(param...)
 }
