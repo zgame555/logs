5b4979856fb00f62e2628a0139e5265f8ad267f5
zgame <premwit.min@gmail.com>
2025-06-14 15:57:46 +0700

    fix : icon

--- logs.go
+++ logs.go
@@ -3,16 +3,16 @@ package logs
 import "fmt"
 
 func Info(param ...any) {
-	param = append([]any{"Info"}, param...)
+	param = append([]any{"✅"}, param...)
 	fmt.Println(param...)
 }
 
 func Error(param ...any) {
-	param = append([]any{"Error"}, param...)
+	param = append([]any{"❌"}, param...)
 	fmt.Println(param...)
 }
 
 func Warn(param ...any) {
-	param = append([]any{"Warn"}, param...)
+	param = append([]any{"⚠️"}, param...)
 	fmt.Println(param...)
 }
